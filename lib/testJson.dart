aa857967f3609f78fcbb56d3543cfcff01410d2d
ittinan99 <61344879+ittinan99@users.noreply.github.com>
2023-05-26 22:58:10 +0700

    something

--- lib/testJson.dart
+++ lib/testJson.dart
@@ -1,8 +1,86 @@
-import 'jsonManager.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-void main() async {
-  Map<String, dynamic> jsonData = await JsonManager.loadJson();
-  
-  print(jsonData);
-  // ทดสอบการใช้งานข้อมูล JSON
+import 'dart:async';
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Hide the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'dbestech',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List _items = [];
+
+  // Fetch content from the json file
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/sample.json');
+    final data = await json.decode(response);
+    setState(() {
+      _items = data["items"];
+      print("..number of items ${_items.length}");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'dbestech',
+        ),
+      ),
+      body:Column(
+        children: [
+          _items.isNotEmpty?Expanded(
+            child: ListView.builder(
+              itemCount: _items.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  key: ValueKey(_items[index]["id"]),
+                  margin: const EdgeInsets.all(10),
+                  color: Colors.amber.shade100,
+                  child: ListTile(
+                    leading: Text(_items[index]["id"]),
+                    title: Text(_items[index]["name"]),
+                    subtitle: Text(_items[index]["description"]),
+                  ),
+                );
+              },
+            ),
+          ): ElevatedButton(
+              onPressed: () {
+                readJson();
+              },
+              child: Center(child: Text("Load Json")))
+        ],
+      ),
+    );
+  }
 }
