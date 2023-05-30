5c30c91a2d291d835385d2895ffb535546129ca9
JINI3Ei <jeerapat.ljee@mail.kmutt.ac.th>
2023-05-30 11:21:01 +0700

    delet useless

--- lib/jsonManager.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert';
-import 'package:flutter/services.dart' show rootBundle;
-
-class JsonManager {
-  static Future<Map<String, dynamic>> loadJson() async {
-    String jsonString = await rootBundle.loadString('assets/dataNike.json');
-    return json.decode(jsonString);
-  }
-}
